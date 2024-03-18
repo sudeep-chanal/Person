26b90a673442f689c35d0b6303c1a1ceeeff4dd1
sudeep-chanal <01fe22bca181@kletech.ac.in>
2024-03-18 11:37:42 +0530

    Program

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+class Person {
+    private String name;
+    private String dateOfBirth;
+
+    public Person(String name, String dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+  
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
+
+
